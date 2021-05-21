341d07e3a0846dfb77f5d62ceb85076547b4a282
breidest-tbs <stephan.breidenbach@solingen.de>
2021-05-21 22:23:55 +0200

    Package.swift manifest file changed

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "pop",
     platforms: [
         //spec.ios.deployment_target = '8.0'
-        .iOS(.v8),
+        .iOS(.v9),
         //spec.osx.deployment_target = '10.8'
         .macOS("10.8")
         //spec.tvos.deployment_target = '9.0'
