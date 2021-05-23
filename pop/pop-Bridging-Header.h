4d2367d08d0a175448fa983b74b0bd21ef039291
breidest-tbs <stephan.breidenbach@solingen.de>
2021-05-23 13:40:37 +0200

    pop bridging header added

---
+++ pop/pop-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  pop-Bridging-Header.h
+//  
+//
+//  Created by Stephan Breidenbach on 23.05.21.
+//
+
+#ifndef POP-BRIDGING-HEADER_h
+#define POP-BRIDGING-HEADER_h
+#import "POP.h"
+
+#endif /* POP-BRIDGING-HEADER_h */
